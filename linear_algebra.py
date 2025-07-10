c2ed3b5e3f028743254cc2c5367fce0186ca8961
starz-0112 <zguo011235@gmail.com>
2025-07-10 17:55:26 +0000

    Added physics functions for AUV

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,15 +1,7 @@
 import numpy as np
 
-# Create vectors
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-
-# Create matrices
-A = np.array([[1, 2], [3, 4]])
-B = np.array([[5, 6], [7, 8]])
-
 # Addition of vectors
-def vector_add(a, b)
+def vector_add(a, b):
     sum_vector = a + b
     print(f"Sum of vectors a and b: {sum_vector}")
 
@@ -19,41 +11,67 @@ def vector_diff(a, b):
     print(f"Difference of vectors a and b: {diff_vector}")
 
 # Dot product of vectors
-def vector_dot_product(a, b)
+def vector_dot_product(a, b):
     dot_product = np.dot(a, b)
     print(f"Dot product of vectors a and b: {dot_product}")
 
 # Addition of matrices
-def matrix_sum(A, B)
+def matrix_sum(A, B):
     sum_matrix = A + B
     print(f"Sum of matrices A and B: {sum_matrix}")
 
 # Subtraction of matrices
-def matrix_diff(A, B)
+def matrix_diff(A, B):
     diff_matrix = A - B
     print(f"Difference of matrices A and B: {diff_matrix}")
 
 # Scalar multiplication of vectors
-def scalar_mult_vector(c, a)
+def scalar_mult_vector(c, a):
     scalar_mult_vector = c * a
     print(f"Scalar multiplication of vector a by {c}: {scalar_mult_vector}")
 
 # Scalar multiplication of matrices
-def scalar_mult(c, A)
+def scalar_mult(c, A):
     scalar_mult_matrix = c * A
     print(f"Scalar multiplication of matrix A by {c}: {scalar_mult_matrix}")
 
 # Matrix multiplication
-def multiply_matrices(A, B)
+def multiply_matrices(A, B):
     product_matrix = np.dot(A, B)
     print(f"Product of matrices A and B: {product_matrix}")
 
 # Transpose of a matrix
-def find_transpose(A)
+def find_transpose(A):
     transpose_matrix = A.T
     print(f"Transpose of matrix A: {transpose_matrix}")
 
 # Magnitude of a matrix
-def find_magnitude (a)
+def find_magnitude (a):
     magnitude = np.linalg.norm(a)
     print(f"Magnitude of vector a: {magnitude}")
+
+# Create vectors
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+f = np.array([1, 1, 2])
+
+# Create matrices
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+C = np.array([[1, 2, 3], [4, 5, 6]])
+D = np.array([[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]])
+
+vector_add(a, b)
+vector_diff(a, b)
+
+matrix_sum(A, B)
+matrix_diff(A, B)
+
+vector_dot_product(a, b)
+
+multiply_matrices(C, D)
+
+find_magnitude(f)
+
+find_transpose(A)
--- physics.py
+++ physics.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 def calculate_buoyancy(V, density_fluid):       #m^3, kg/m^3
     """Write a Python function, calculate_buoyancy, that calculates the buoyancy force exerted on a object submerged in water."""
     g = 9.81        #m/s^2
@@ -12,4 +15,31 @@ def will_it_float(V, mass):     #m^3, kg
         return False        #sink
     else:
         return True     #float
+    
+def calculate_pressure(depth):
+    """Write a Python function, calculate_pressure, that calculates the pressure at a given depth in water."""
+    g = 9.81
+    return depth*1000*g
+
+def calculate_acceleration(F, m)        #Newtons, kg
+    """Write a Python function, calculate_acceleration, that calculates the acceleration of an object"""
+    return F/m
+
+def calculate_angular_acceleration(tau, I)      #torque in Newton-meters, intertia in kg*m^2
+    """Write a Python function, calculate_angular_acceleration, that calculates the angular acceleration of an object given the torque applied to it and its moment of inertia."""
+    return tau/I
+
+def calclate_torque(F_magnitude, F_degrees, r)      #Newtons, degrees, meters
+    """Write a Python function, calculate_torque, that calculates the torque applied to an object given the force applied to it and the distance from the axis of rotation to the point where the force is applied."""
+    return F_magnitude*r*sin(F_degrees)
+
+def calculate_moment_of_inertia(m, r)       #kilograms, meters
+    """Write a Python function, calculate_moment_of_inertia, that calculates the moment of inertia of an object given its mass and the distance from the axis of rotation to the center of mass of the object."""
+    return m*r*r
+
+
+def calculate_auv_acceleration(F_magnitude, F_angle, mass, volume, thruster_distance)     #Newtons, radians, kg, m^3, m
+
+
+#calculate_auv_acceleration(100, math.pi/6, 100, 0.1, 0.5)
     
